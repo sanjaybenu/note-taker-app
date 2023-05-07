eb91616c4833ab15dc778986d5774a76484ef5fd
S Chopra <sanjaybenu@gmail.com>
2023-05-07 14:07:08 +1000

    changed deletion function to make it work on heroku deployment

--- helpers/notes.js
+++ helpers/notes.js
@@ -20,16 +20,37 @@ const writeNewNote = (body, notesObj) => {
 }
 
 // function to rewrite notes.json file after note removal
-const removeNote = (id, notesObj) => {
-    notesObj = notesObj.filter(note => note.id !== id);
-    fs.writeFile(path.join(__dirname, '../db/notes.json'), JSON.stringify(notesObj, null, 2),(err)=>{
-        if (err){
+// const removeNote = (id, notesObj) => {
+//     notesObj = notesObj.filter(note => note.id !== id);
+//     fs.writeFile(path.join(__dirname, '../db/notes.json'), JSON.stringify(notesObj, null, 2),(err)=>{
+//         if (err){
 
-            console.log('error')
-        }
-    });
+//             console.log('error')
+//         }
+//     });
     
+// };
+
+// revised delete function to current state of notes file and then filter and writeupdated notes
+const removeNote = (id, notesObj) => {
+  fs.readFile(path.join(__dirname, './db/notes.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    let notes = JSON.parse(data);
+    notes = notesObj.filter(note => note.id !== id);
+
+    fs.writeFile(path.join(__dirname, './db/notes.json'), JSON.stringify(notes, null, 2), (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    });
+  });
 };
+
 
 // exporting functions
 module.exports = { writeNewNote, removeNote };
