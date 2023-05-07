3c921e8fe10beb60c73cc0c5dfaa0ae8efc81876
S Chopra <sanjaybenu@gmail.com>
2023-05-07 14:14:36 +1000

    reversing changes as the new changes do not work

--- helpers/notes.js
+++ helpers/notes.js
@@ -20,36 +20,18 @@ const writeNewNote = (body, notesObj) => {
 }
 
 // function to rewrite notes.json file after note removal
-// const removeNote = (id, notesObj) => {
-//     notesObj = notesObj.filter(note => note.id !== id);
-//     fs.writeFile(path.join(__dirname, '../db/notes.json'), JSON.stringify(notesObj, null, 2),(err)=>{
-//         if (err){
-
-//             console.log('error')
-//         }
-//     });
-    
-// };
-
-// revised delete function to current state of notes file and then filter and writeupdated notes
 const removeNote = (id, notesObj) => {
-  fs.readFile(path.join(__dirname, '../db/notes.json'), 'utf8', (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
+    notesObj = notesObj.filter(note => note.id !== id);
+    fs.writeFile(path.join(__dirname, '../db/notes.json'), JSON.stringify(notesObj, null, 2),(err)=>{
+        if (err){
 
-    let notes = JSON.parse(data);
-    notes = notesObj.filter(note => note.id !== id);
-
-    fs.writeFile(path.join(__dirname, '../db/notes.json'), JSON.stringify(notes, null, 2), (err) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
+            console.log('error')
+        }
     });
-  });
+    
 };
+
+
 
 
 // exporting functions
