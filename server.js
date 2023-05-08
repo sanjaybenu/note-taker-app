e9d5d3f3d4f16996bb2714659290dabd828fe1f3
S Chopra <sanjaybenu@gmail.com>
2023-05-08 21:19:54 +1000

    added cors

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const PORT = process.env.PORT || 3001;
 // importing npm express package
 const express = require('express');
 // declaring variable app
+const cors = require('cors')
 const app = express();
 //importing routes
+app.use(cors())
 const htmlRoutes = require('./routes/htmlRoutes')
 const apiRoutes = require('./routes/apiRoutes')
 // middleware for parsing
@@ -12,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //middleware for public folder and routes
 app.use(express.static('public'));
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 // initialising Server
